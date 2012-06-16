24ce798dbcb14f15b7a708595388c7873608a3a2
Igorek Popov <foetano@gmail.com>
2012-06-16 14:00:25 +0400

    Done with user edit/update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
+  before_filter :sign_in,      :only => [:new, :create]
 
   def index
     @title = "All users"
@@ -64,6 +65,10 @@ class UsersController < ApplicationController
 	
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+	
+    def sign_in
+      redirect_to(root_path) if signed_in?
     end
 	
 end
